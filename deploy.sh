6e2705248f321737003d111886fcb12bc65ecb88
Mark Dewey <mark.t.dewey@gmail.com>
2019-10-01 16:15:03 -0400

    updated docker file

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,7 @@ docker build -t sdg-words-image ./bin/release/netcoreapp3.0/publish
 
 docker tag sdg-words-image registry.heroku.com/sdg-words/web
 
+docker push registry.heroku.com/sdg-words/web
 
-# docker push registry.heroku.com/sdg-words/web
-
-# heroku container:release web -a sdg-words
-docker run -p 4000:80 sdg-words-image 
+heroku container:release web -a sdg-words
+# docker run -p 4000:80 sdg-words-image 
